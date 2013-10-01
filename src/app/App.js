f6df357f1c612eb1023a2afe9c489b937adfbd81
steveoh <sgourley@utah.gov>
2013-10-01 10:22:30 -0600

    Added the report generator to the app.
    Removed printing. Converted all quotes to js style single quotes. Removed embedd stuff. Removed unused requires and organized.
     Made the sidebar bigger to fit the wizard buttons.

--- src/app/App.js
+++ src/app/App.js
@@ -1,167 +1,143 @@
 define([
-    'dijit/registry', 
-    'dojo/dom', 
-    'dojo/_base/declare',
-    'dijit/_WidgetBase', 
-    'dijit/_TemplatedMixin', 
-    'dijit/_WidgetsInTemplateMixin',
-    'dojo/text!app/templates/App.html',
-    'agrc/widgets/map/BaseMap',
-    // 'ijit/modules/ErrorLogger',
-    'ijit/widgets/layout/SideBarToggler',
-    'agrc/widgets/locate/FindAddress',
-    'agrc/widgets/locate/MagicZoom',
-    'agrc/widgets/map/BaseMapSelector',
-    'esri/dijit/Print',
-    'dojo/dom-style',
+        'dojo/text!app/templates/App.html',
 
-    'dijit/layout/BorderContainer',
-    'dijit/layout/ContentPane'
-], 
+        'dojo/_base/declare',
+        
+        'dijit/_WidgetBase',
+        'dijit/_TemplatedMixin',
+        'dijit/_WidgetsInTemplateMixin',
 
-function (
-    registry, 
-    dom, 
-    declare, 
-    _WidgetBase, 
-    _TemplatedMixin, 
-    _WidgetsInTemplateMixin, 
-    template, 
-    BaseMap, 
-    // ErrorLogger, 
-    SideBarToggler, 
-    FindAddress,
-    MagicZoom,
-    BaseMapSelector,
-    Print,
-    domStyle
+        'agrc/widgets/map/BaseMap',
+        'agrc/widgets/locate/FindAddress',
+        'agrc/widgets/locate/MagicZoom',
+        'agrc/widgets/map/BaseMapSelector',
+
+        'ijit/widgets/layout/SideBarToggler',
+
+        'app/reportGeneratorWizard',
+
+        'dijit/layout/BorderContainer',
+        'dijit/layout/ContentPane'
+    ],
+
+    function(
+        template,
+        
+        declare,
+
+        _WidgetBase,
+        _TemplatedMixin,
+        _WidgetsInTemplateMixin,
+        
+        BaseMap,
+        FindAddress,
+        MagicZoom,
+        BaseMapSelector,
+
+        SideBarToggler,
+
+        Wizard
     ) {
-    return declare("app/App", 
-        [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], 
-        {
-        // summary:
-        //      The main widget for the app
-
-        widgetsInTemplate: true,
-        templateString: template,
-        baseClass: 'app',
-
-        // map: agrc.widgets.map.Basemap
-        map: null,
-        
-        constructor: function(){
+        return declare('app/App', [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
             // summary:
-            //      first function to fire after page loads
-            console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
+            //      The main widget for the app
 
-            // AGRC.errorLogger = new ErrorLogger({appName: 'ProjectName'});
+            widgetsInTemplate: true,
+
+            templateString: template,
             
-            AGRC.app = this;
+            baseClass: 'app',
 
-            this.inherited(arguments);
-        },
-        postCreate: function () {
-            // summary:
-            //      Fires when 
-            console.log(this.declaredClass + "::" + arguments.callee.nom, arguments);
-        
-            // set version number
-            this.version.innerHTML = AGRC.version;
+            // map: agrc.widgets.map.Basemap
+            map: null,
 
-            this.inherited(arguments);
-        },
-        startup: function () {
-            // summary:
-            //      Fires after postCreate when all of the child widgets are finished laying out.
-            console.log(this.declaredClass + "::" + arguments.callee.nom, arguments);
+            constructor: function() {
+                // summary:
+                //      first function to fire after page loads
+                console.info(this.declaredClass + '::' + arguments.callee.nom, arguments);
 
-            // call this before creating the map to make sure that the map container is 
-            // the correct size
-            this.inherited(arguments);
-            
-            var sb;
-            var fa;
-            var fp;
-            var fm;
+                AGRC.app = this;
 
-            this.initMap();
-            
-            sb = new SideBarToggler({
-                sidebar: this.sideBar.domNode,
-                mainContainer: this.mainContainer,
-                map: this.map,
-                centerContainer: this.centerContainer.domNode
-            }, this.sidebarToggle);
+                this.inherited(arguments);
+            },
+            postCreate: function() {
+                // summary:
+                //      Fires when 
+                console.log(this.declaredClass + '::' + arguments.callee.nom, arguments);
 
-            fa = new FindAddress({
-                map: this.map,
-                apiKey: AGRC.apiKey
-            }, this.geocodeNode);
+                // set version number
+                this.version.innerHTML = AGRC.version;
 
-            fp = new MagicZoom({
-                map: this.map,
-                mapServiceURL: AGRC.urls.vector,
-                searchLayerIndex: 4,
-                searchField: 'NAME',
-                placeHolder: 'place name...',
-                maxResultsToDisplay: 10,
-                'class': 'first'
-            }, this.gnisNode);
+                this.inherited(arguments);
+            },
+            startup: function() {
+                // summary:
+                //      Fires after postCreate when all of the child widgets are finished laying out.
+                console.log(this.declaredClass + '::' + arguments.callee.nom, arguments);
 
-            fm = new MagicZoom({
-                map: this.map,
-                mapServiceURL: AGRC.urls.vector,
-                searchLayerIndex: 1,
-                searchField: 'NAME',
-                placeHolder: 'city name...',
-                maxResultsToDisplay: 10
-            }, this.cityNode);
+                // call this before creating the map to make sure that the map container is 
+                // the correct size
+                this.inherited(arguments);
 
-            this.inherited(arguments);
+                var toggler,
+                    geocoder,
+                    zoomer,
+                    cityZoomer,
+                    wizard;
 
-            this.printer = new Print({
-                map: this.map,
-                url: AGRC.exportWebMapUrl,
-                templates: [
-                    {
-                        label: 'Portrait (PDF)',
-                        format: 'PDF',
-                        layout: 'Letter ANSI A Portrait',
-                        options: {
-                            legendLayers: []
-                        }
-                    },{
-                        label: 'Landscape (PDF)',
-                        format: 'PDF',
-                        layout: 'Letter ANSI A Landscape',
-                        options: {
-                            legendLayers: []
-                        }
-                    }
-                ]
-            }, this.printDiv);
-            this.printer.startup();
-            var that = this;
-            this.printer.on('print-complete', function () {
-                domStyle.set(that.popupBlurb, 'display', 'block');
-            });
-        },
-        initMap: function(){
-            // summary:
-            //      Sets up the map
-            console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
-            
-            this.map = new BaseMap(this.mapDiv, {
-                useDefaultBaseMap: false
-            });
+                this.initMap();
 
-            var s;
+                toggler = new SideBarToggler({
+                    sidebar: this.sideBar.domNode,
+                    mainContainer: this.mainContainer,
+                    map: this.map,
+                    centerContainer: this.centerContainer.domNode
+                }, this.sidebarToggle);
 
-            s = new BaseMapSelector({
-                map: this.map,
-                id: 'claro',
-                position: 'TR'
-            });
-        }
+                geocoder = new FindAddress({
+                    map: this.map,
+                    apiKey: AGRC.apiKey
+                }, this.geocodeNode);
+
+                zoomer = new MagicZoom({
+                    map: this.map,
+                    mapServiceURL: AGRC.urls.vector,
+                    searchLayerIndex: 4,
+                    searchField: 'NAME',
+                    placeHolder: 'place name...',
+                    maxResultsToDisplay: 10,
+                    'class': 'first'
+                }, this.gnisNode);
+
+                cityZoomer = new MagicZoom({
+                    map: this.map,
+                    mapServiceURL: AGRC.urls.vector,
+                    searchLayerIndex: 1,
+                    searchField: 'NAME',
+                    placeHolder: 'city name...',
+                    maxResultsToDisplay: 10
+                }, this.cityNode);
+
+                wizard = new Wizard({}, this.reportNode);
+
+                this.inherited(arguments);
+            },
+            initMap: function() {
+                // summary:
+                //      Sets up the map
+                console.info(this.declaredClass + '::' + arguments.callee.nom, arguments);
+
+                this.map = new BaseMap(this.mapDiv, {
+                    useDefaultBaseMap: false
+                });
+
+                var selector;
+
+                selector = new BaseMapSelector({
+                    map: this.map,
+                    id: 'claro',
+                    position: 'TR'
+                });
+            }
+        });
     });
-});
